41b28bb09ee48afb5aaa707477480c80468f8fef
Jullira <32617407+Jullira@users.noreply.github.com>
2019-03-17 20:04:57 +0000

    Get request er komið!

--- app/src/main/java/com/softwareproject2/hi/lilbill/MainActivity.java
+++ app/src/main/java/com/softwareproject2/hi/lilbill/MainActivity.java
@@ -1,24 +1,37 @@
 package com.softwareproject2.hi.lilbill;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Application;
-import android.content.res.Resources;
 import android.util.Log;
 import android.widget.TextView;
-import com.android.volley.Request;
+import android.widget.Toast;
+
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.softwareproject2.hi.lilbill.MainActivity;
-import com.softwareproject2.hi.lilbill.R;
 
+
+import okhttp3.OkHttpClient;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Request;
+import okhttp3.Response;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.json.JSONObject;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
 
 
@@ -27,34 +40,65 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final String url = "https://apis.is/concerts";
-//    RequestQueue ExampleRequestQueue = Volley.newRequestQueue(this);
 
+        getData();
+    }
 
-        // prepare the Request
-        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>()
-                {
-                    @Override
-                    public void onResponse(JSONObject response) {
+    private void getData() {
+        String apiKey = "a6bcdd96c91df987a3833e64d05b5987";
+        String url = "https://apis.is/concerts";
 
-                        String json = String.valueOf(response);
-                        String string = "dsadsa";
+        if (isNetworkAvailable()) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
 
-                        int bla = 1232;
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
 
-                        String strFormat = getResources().getString(R.string.json_response);
-                        TextView.setText(strFormat);
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Log.i(TAG, "failed!");
+                        }
+                    });
+                }
 
-                    }
-                },
-                new Response.ErrorListener()
-                {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.d("Error.Response", String.valueOf(error));
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+
+                    try {
+                        final String jsonData = response.body().string();
+
+                        runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                Resources res = getResources();
+                                TextView textView = findViewById(R.id.json_view);
+                                String text = String.format(res.getString(R.string.json_response), jsonData);
+                                textView.setText(text);
+                            }
+                        });
+
+                        Log.v(TAG, jsonData);
+
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught: ", e);
                     }
                 }
-        );
+            });
+        }
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if(networkInfo!= null && networkInfo.isConnected()) isAvailable = true;
+        return isAvailable;
     }
 }
