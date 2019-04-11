ac457cc5336afc5beb65d902bf812557b3574491
Jullira <32617407+Jullira@users.noreply.github.com>
2019-04-11 00:47:22 +0000

    get transaction inn í get account

--- app/src/main/java/com/softwareproject2/hi/lilbill/Get.java
+++ app/src/main/java/com/softwareproject2/hi/lilbill/Get.java
@@ -92,6 +92,53 @@ public class Get {
 
     }
 
+    public Transaction getTransaction(String url) throws IOException {
+
+        if (isNetworkAvailable()) {
+            // Establish connection
+            try {
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+                Response responses = null;
+
+                try {
+                    responses = client.newCall(request).execute();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String jsonData = responses.body().string();
+
+//                Log.d(TAG, "line 62" + jsonData);
+
+                JsonObject jsonObject = new JsonParser().parse(jsonData).getAsJsonObject();
+
+                final String mId = jsonObject.get("id").getAsString();
+                final String mAccountId = jsonObject.get("accountId").getAsString();
+                final String mAmountString = jsonObject.get("amount").getAsString();
+                final Float mAmount = Float.parseFloat(mAmountString);
+                final String mDescr = jsonObject.get("descr").getAsString();
+                final String mDate = jsonObject.get("date").getAsString();
+
+                Transaction transaction = new Transaction();
+                transaction.setId(mId);
+                transaction.setAccountId(mAccountId);
+                transaction.setAmount(mAmount);
+                transaction.setDescription(mDescr);
+                transaction.setDate(mDate);
+
+                return transaction;
+
+            } catch (IOException e) {
+
+            }
+        }
+
+        return null;
+
+    }
+
     public Account getAccountData(String url) throws IOException {
 
         /* Strengur sem er url sem samsvarar hvert á að sækja gögn
@@ -130,10 +177,9 @@ public class Get {
                 final List<Transaction> mTransactionList = new ArrayList<>();
 
                 for(int i = 0; i < transactions.length; i++) {
-                    Transaction transaction = new Transaction();
-                    transaction.setDescription("bla");
-                    mTransactionList.add(transaction);
+                    String transUrl = "https://lilbill.herokuapp.com/user/2/transaction/" + i;
 
+                    mTransactionList.add(getTransaction(transUrl));
                 }
 
                 Account account = new Account();
