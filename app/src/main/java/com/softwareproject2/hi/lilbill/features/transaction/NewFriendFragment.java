ea7e1d5c431d8575ad0bfd71b7ed4cfb5da41c7a
fridasnaedis <fridasnaedis@gmail.com>
2019-04-11 01:58:23 +0000

    Make pretty

--- app/src/main/java/com/softwareproject2/hi/lilbill/features/transaction/NewFriendFragment.java
+++ app/src/main/java/com/softwareproject2/hi/lilbill/features/transaction/NewFriendFragment.java
@@ -32,7 +32,7 @@ public class NewFriendFragment extends Fragment {
         mSubmitButton = (Button) v.findViewById(R.id.submit_new_friend);
         mSubmitButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Toast toast = Toast.makeText(getActivity(), "This has not been implemented yet", Toast.LENGTH_SHORT);
+                Toast toast = Toast.makeText(getActivity(), "This has ginot been implemented yet", Toast.LENGTH_SHORT);
                 toast.show();
                 // TODO: Do the stuff here
                 getActivity().finish();
